e103a149b170edcc11630ffc0b45b4563dd37389
Malek Mansour <malek24mansour@gmail.com>
2025-04-17 14:52:45 -0600

    Add Controls.js

---
+++ games/blackjack/src/components/Controls.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Controls({ onHit, onStand, disabled }){
+  return (
+    <div className='controls'>
+      <button onClick={onHit} disabled={disabled}>Hit</button>
+      <button onClick={onStand} disabled={disabled}>Stand</button>
+    </div>
+  )
+}
