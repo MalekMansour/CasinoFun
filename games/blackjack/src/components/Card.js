8714eca7467dd4961d0d7fe728db0a99b5f56648
Malek Mansour <malek24mansour@gmail.com>
2025-04-17 14:52:45 -0600

    Add Card.js

---
+++ games/blackjack/src/components/Card.js
@@ -0,0 +1,21 @@
+// src/components/Card.js
+import React from 'react'
+
+export default function Card({ card }) {
+  // red for hearts & diamonds, black otherwise
+  const color = card.suit === '♥' || card.suit === '♦' ? 'red' : 'black'
+
+  return (
+    <div className="card" style={{ color }}>
+      <div className="corner">
+        <span>{card.rank}{card.suit}</span>
+      </div>
+      <div className="center">
+        <span style={{ fontSize: '1.5rem' }}>{card.suit}</span>
+      </div>
+      <div className="corner flip">
+        <span>{card.rank}{card.suit}</span>
+      </div>
+    </div>
+  )
+}
