03d76dd1ea699603429ae0741c149d3148c17779
Malek Mansour <malek24mansour@gmail.com>
2025-04-18 13:47:15 -0600

    Update Hand.js

--- games/blackjack/src/components/Hand.js
+++ games/blackjack/src/components/Hand.js
@@ -4,7 +4,6 @@ import { getHandValue } from '../utils/game';
 
 export default function Hand({ cards, title, hideHoleCard = false }) {
   const displayValue = hideHoleCard ? '?' : getHandValue(cards);
-
   return (
     <div className="hand">
       <h2>{title} ({displayValue})</h2>
