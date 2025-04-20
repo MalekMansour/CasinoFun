02ff64463e805ca9bc91d8d6d25f080ce89fc57b
Malek Mansour <malek24mansour@gmail.com>
2025-04-20 02:21:08 -0600

    Update Hand.js

--- games/blackjack/src/components/Hand.js
+++ games/blackjack/src/components/Hand.js
@@ -2,17 +2,18 @@ import React from 'react';
 import Card from './Card';
 import { getHandValue } from '../utils/game';
 
-export default function Hand({ cards, title, hideHoleCard = false }) {
-  const displayValue = hideHoleCard ? '?' : getHandValue(cards);
+export default function Hand({ cards, title, hideHoleCard=false }) {
+  const display = hideHoleCard ? '?' : getHandValue(cards);
   return (
     <div className="hand">
-      <h2>{title} ({displayValue})</h2>
+      <h2>{title} ({display})</h2>
       <div className="cards">
-        {cards.map((c, i) => (
+        {cards.map((c,i)=>(
           <div key={i}>
-            {i === 1 && hideHoleCard
+            {i===1 && hideHoleCard
               ? <div className="card back"></div>
-              : <Card card={c} />}
+              : <Card card={c}/>
+            }
           </div>
         ))}
       </div>
