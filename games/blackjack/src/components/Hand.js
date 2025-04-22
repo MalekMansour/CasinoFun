9729d1f38f68fd01e0bb340bae0bbd0146c6899c
Malek Mansour <malek24mansour@gmail.com>
2025-04-21 20:01:55 -0600

    Update Hand.js

--- games/blackjack/src/components/Hand.js
+++ games/blackjack/src/components/Hand.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Card from './Card';
+import Card from 'Card';
 import { getHandValue } from '../utils/game';
 
 export default function Hand({ cards, title, hideHoleCard=false }) {
