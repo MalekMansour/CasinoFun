6c7091f1105916ac8e24d6bbc17899223ec04753
Malek Mansour <malek24mansour@gmail.com>
2025-07-07 19:59:23 -0600

    Update mod2.lua

--- cyberpunk mods/mod2.lua
+++ cyberpunk mods/mod2.lua
@@ -1,21 +1,17 @@
--- Cyberpunk Mod: Give $1 to player every time 'W' is pressed
+-- Simple terminal version: Give $1 to player every time 'w' is entered
 
 local playerMoney = 0
 
--- Function to add money
-local function giveMoney(amount)
-    playerMoney = playerMoney + amount
-    print("You received $1! Total money: $" .. playerMoney)
-end
-
--- Key press handler
-function love.keypressed(key)
-    if key == "w" then
-        giveMoney(1)
+while true do
+    io.write("Press 'w' and Enter to get $1 (or 'q' to quit): ")
+    local input = io.read()
+    if input == "w" then
+        playerMoney = playerMoney + 1
+        print("You received $1! Total money: $" .. playerMoney)
+    elseif input == "q" then
+        print("Goodbye! Final money: $" .. playerMoney)
+        break
+    else
+        print("Invalid input.")
     end
-end
-
--- Optional: Draw money on screen (if using LÖVE framework)
-function love.draw()
-    love.graphics.print("Money: $" .. playerMoney, 10, 10)
 end
